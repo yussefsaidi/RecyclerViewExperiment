3dedcc040260a96f0e66d73a58bc4db1db7bd861
Yussef <saidi.yussef@live.fr>
2020-01-04 13:46:57 -0500

    add possibility to add elements to RecyclerView

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # RecyclerViewExperiment
+App to display long scrollable list of words. This is created using a RecyclerView, an Adapter, and a list of data.
--- app/src/main/java/com/saidiyussef/recyclerview/MainActivity.java
+++ app/src/main/java/com/saidiyussef/recyclerview/MainActivity.java
@@ -36,8 +36,13 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                int wordListSize = mWordList.size();
+                // Add a new word to the wordList
+                mWordList.addLast("+ Word " + wordListSize);
+                // Notify the adapter, that the data has changed
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+                // Scroll to the bottom
+                mRecyclerView.smoothScrollToPosition(wordListSize);
             }
         });
 
